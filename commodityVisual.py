469d22f98eb52c06ad4ce59e5f413a029d348c07
Hughft <zv21394@bristol.ac.uk>
2025-03-04 15:56:25 +0000

    start of a random forests model

--- commodityVisual.py
+++ commodityVisual.py
@@ -2,11 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-df = pd.read_csv("commodity_futures.csv", parse_dates=["Date"])
+df = pd.read_csv("hugh_working_folder/commodity_futures_adjusted/commodity_futures_percentage_returns.csv", parse_dates=["Date"])
 df.set_index("Date", inplace=True)
 commodities = df.columns
 for commodity in commodities:
-    if commodity == 'WTI CRUDE' or commodity == 'BRENT CRUDE': #set commodities to compare
+    if commodity == 'GOLD': #set commodities to compare
         if df[commodity].notna().sum() == 0:
             continue
         fig, ax = plt.subplots(figsize=(8, 4))
---
+++ hugh_working_folder/randomForest.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+commodity_futures_differencing = 'hugh_working_folder/commodity_futures_adjusted/commodity_futures_differencing.csv'
+commodity_futures_log_returns = 'hugh_working_folder/commodity_futures_adjusted/commodity_futures_log_returns.csv'
+commodity_futures_percentage_returns = 'hugh_working_folder/commodity_futures_adjusted/commodity_futures_percentage_returns.csv'
+
+df = pd.read_csv(commodity_futures_log_returns, parse_dates=['Date'], index_col='Date')
+
