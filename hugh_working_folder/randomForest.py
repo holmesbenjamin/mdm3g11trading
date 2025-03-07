2617057e1727131236df9a33e6a05cda7f18b66e
Hughft <zv21394@bristol.ac.uk>
2025-03-07 14:20:31 +0000

    building the baseline for a random forest model

--- hugh_working_folder/randomForest.py
+++ hugh_working_folder/randomForest.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
 commodity_futures_differencing = 'hugh_working_folder/commodity_futures_adjusted/commodity_futures_differencing.csv'
 commodity_futures_log_returns = 'hugh_working_folder/commodity_futures_adjusted/commodity_futures_log_returns.csv'
 commodity_futures_percentage_returns = 'hugh_working_folder/commodity_futures_adjusted/commodity_futures_percentage_returns.csv'
